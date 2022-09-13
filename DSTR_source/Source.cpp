457a2e4bdeed317e62efe89ba31ba0214ddbfdd9
tzubindev <71303907+tzubindev@users.noreply.github.com>
2022-09-13 21:28:03 +0800

    stack [update]

--- DSTR_source/Source.cpp
+++ DSTR_source/Source.cpp
@@ -12,3 +12,18 @@ using namespace std;
 
 
 // stack
+template <class T>
+
+class Stack {
+	T* list;
+	int stackTop;
+	int maxStackSize;
+
+public:
+	Stack(int MaxSize);
+	~Stack();
+	void push(T elem);
+	T pop();
+	T top();
+	bool isEmpty();
+};
