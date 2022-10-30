862a0f331bd130f3cf68a5dde82e5d730a7afeee
ghassan <Ghassanyjr@gmail.com>
2022-10-30 12:17:04 +0800

    Updated Liked list

--- DSTR_source/Test.h
+++ DSTR_source/Test.h
@@ -1,13 +1,17 @@
+
+
 #include <iostream>
 #include "AdminMenu.h"
+#include "LinkedList.h"
+
 
 using namespace std;
 
 void Test()
 {
-	
+
 	// Build Storage
-	TemporaryStorage* storage = new TemporaryStorage();
+	/*TemporaryStorage* storage = new TemporaryStorage();
 	Menu *mainMenu = new Menu();
 
 	while (!mainMenu->getExitStatus()) {
@@ -19,6 +23,19 @@ void Test()
 		else {
 
 			cout << 'P';
+			break;
 		}
-	}
+	}*/
+
+	LinkedList<int> lst;
+
+	lst.insertAtbeginning(1);
+	lst.insertAtbeginning(2);
+	lst.insertAtbeginning(3);
+	lst.insertAtbeginning(4);
+	lst.insertAtbeginning(5);
+	lst.insertAtbeginning(6);
+
+	cout << lst.getItem(3);
+
 }
